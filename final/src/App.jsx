4aefb2de91e99ded39c1f548af4a11b37d9a5ac4
Selvaraj0211 <selvarannav6231@gmail.com>
2025-07-04 22:07:55 +0530

    m

--- final/src/App.jsx
+++ final/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import {BrowserRouter, Routes, Route} from'react-router-dom'
-import Home from '../pages/Home'
+import Home from './Pages/Home'
 import Product from './components/Product'
 import Stool from './productpage/Stools'
 import { ProductProvider } from './context/ProductContext'
